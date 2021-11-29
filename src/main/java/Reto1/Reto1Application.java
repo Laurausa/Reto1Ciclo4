157bd9b33fa5b66e0e0dee6f79e8f2c7acc31714
laurausa <laura.melo1.mt@correo.usa.edu.co>
2021-11-29 14:47:11 -0500

    first commit

---
+++ src/main/java/Reto1/Reto1Application.java
@@ -0,0 +1,20 @@
+//modelo
+//interface
+//repositorio
+//Servicio
+//Controlador
+
+
+package Reto1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Reto1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Reto1Application.class, args);
+	}
+
+}
